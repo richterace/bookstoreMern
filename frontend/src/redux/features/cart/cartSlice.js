f3dcb92c00cbcf3a4348792ac3e1dff08b777431
Richter Ace G. Villas <villasace098@gmail.com>
2025-08-11 20:57:35 +0800

    sweet alert implemented

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import Swal from 'sweetalert2';
 
 
 const initialState = {
@@ -13,14 +14,34 @@ const cartSlice = createSlice({
             const existingItem = state.cartItems.find(item => item._id == action.payload._id); //checking if there's added item on the cart
             if (!existingItem) {
                 state.cartItems.push(action.payload);
-                alert("Item added successfully")
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Product Added",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
             } else (
-                alert("Item already existed")
+                Swal.fire({
+                    title: "Item already added",
+                    text: "You should pick different one!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Okay!"
+                })
             )
+        },
+        removeFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter(item => item._id !== action.payload._id)
+        },
+        clearCart: (state) => {
+            state.cartItems = []
         }
     }
 })
 
 // export the action
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
 export default cartSlice.reducer
