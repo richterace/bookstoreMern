3835502fa1389e2c7f975ff245faef48f60e3390
richterace <villasace098@gmail.com>
2025-09-03 16:22:19 +0800

    test add book

--- frontend/src/pages/dashboard/addBook/AddBook.jsx
+++ frontend/src/pages/dashboard/addBook/AddBook.jsx
@@ -14,6 +14,23 @@ const AddBook = () => {
     const [addBook, {isLoading, isError}] = useAddBookMutation()
     const [imageFileName, setimageFileName] = useState('')
 
+
+    // const uploadImageToCloudinary = async (file) => {
+    //     const formData = new FormData();
+    //     formData.append('file', file);
+    //     formData.append('upload_preset', 'your_upload_preset'); // Set this up in your Cloudinary dashboard
+
+    //     const res = await fetch('https://api.cloudinary.com/v1_1/your_cloud_name/image/upload', {
+    //         method: 'POST',
+    //         body: formData,
+    //     });
+
+    //     const data = await res.json();
+    //     return data.secure_url; // This is the image URL you can use
+    //     };
+
+    // use the above method when you want to deal with dynamic uploading.
+
     const onSubmit = async (data) => {
  
         const newBookData = {
